24c8baaa4c4d3f06edbd39bcec820115a1d4fefd
Karina Chow <karina@qtalo.com>
2024-03-25 20:43:28 -0400

    Killed ZAnimation* start Timeline class

--- Timeline.cpp
+++ Timeline.cpp
@@ -1,16 +1,27 @@
+#include <Adafruit_NeoPixel.h>
 #include "Arduino.h"
 #include "Timeline.h"
 
-Timeline::Timeline(int bpm) {
+Timeline::Timeline() {
   int currentTime = millis();
-  
+  std::map<std::string, Adafruit_NeoPixel> strips = {};
+}
+
+void addLEDAccessory(std::string name, uint8_t pin = 8, uint8_t numLEDs = 262) {
 
 }
 
-void Timeline::addAnimation(int startBeat, int endBeat){ // enum animation
+void Timeline::addLEDAccessory(std::string name, uint8_t pin, uint8_t numLEDs) {
+  strips[name] = Adafruit_NeoPixel(numLEDs, pin, NEO_GRB + NEO_KHZ800);
+}
+
+// void Timeline::addRoutine(int bpm, Animation[] animations) {
+// }
+
+void Timeline::addAnimation(int startBeat, int endBeat, Adafruit_NeoPixel light) {
 
 }
 
-void Timeline::playAnimation(){
-  
+void Timeline::playAnimation() {
+
 }
