9e829f0864e03aeaf020078f548666f3adc76be1
DanNoble <danielwai999@gmail.com>
2024-03-27 22:22:19 -0400

    Updated light fade to be between 2 colors

--- ZAnimation.cpp
+++ ZAnimation.cpp
@@ -29,7 +29,7 @@ void Animation::blink(uint64_t color) {
     }
 }
 
-void Animation::lightFade(uint32_t color, long duration) { //duration in miliiseconds
+void lightFade(uint32_t color, long duration) {
   long startTime = millis();
   long currentTime = millis() % startTime;
   while(currentTime < duration){ 
@@ -39,3 +39,29 @@ void Animation::lightFade(uint32_t color, long duration) { //duration in miliise
     strip.setBrightness(l);
     strip.show();
   };
+}
+
+void Animation::lightFade2(uint32_t color, uint32_t color2, long duration) {
+  long startTime = millis();
+  long currentTime = millis() % startTime;
+
+  uint8_t r1 = color >> 16;
+  uint8_t r2 = color2 >> 16;
+
+  uint8_t g1 = color >> 8;
+  uint8_t g2 = color2 >> 8;
+
+  uint8_t b1 = color;
+  uint8_t b2 = color2;
+
+  while(currentTime < duration){ 
+    int currentTime = millis() - startTime;
+
+    int r = map(currentTime, 0, duration, r1, r2);
+    int g = map(currentTime, 0, duration, g1, g2);
+    int b = map(currentTime, 0, duration, b1, b2);
+
+    strip.fill(strip.Color(r,g,b));
+    strip.show();
+  };
+};
